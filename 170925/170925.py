f16a8dabd205b519fe8ae8a1a0c2a51d02925712
rnaster <blpomspo@gmail.com>
2017-09-26 19:08:11 +0900

    python 과제1, 3

--- 170925/170925.py
+++ 170925/170925.py
@@ -30,18 +30,27 @@ parser.feed(html)
 
 import re
 string1 = 'Earth is the third planet from the Sun'
-pat = re.compile(r'(\w{2})([\w]+| )', re.I)
-result = re.findall(pat, string1)
-# for i,_ in result:
-#     print(i, end=' ')
+pat = re.compile(r'\b\w{2}', re.I)
+# print(re.findall(pat, string1))
 
 string2 = 'abc.test@gmail.com, xyz@test.in, test.first@analyicsvidhya.com, first@rest.biz'
-pat = re.compile(r'@[\w.,]+')
+pat = re.compile(r'@[\w.]+')
 # print(re.findall(pat, string2))
 
-string3 = 'Amit 34-3456 12-05-2007, XYZ 56-4532 11-11-2011, ABC 67-8945 12-01-2009'
-pat = re.compile(r'\d{2}-\d{2}-\d{4}')
+
+# string3 = 'Amit 34-3456 12-05-2007, XYZ 56-4532 11-11-2011, ABC 67-8945 12-01-2009'
+# pat = re.compile(r'\d{2}-\d{2}-\d{4}')
 # print(re.findall(pat, string3))
 
 string4 = 'Earth`s gravity interacts with other objects in space, especially the Sun and the Moon.'
-# pat = re.compile(r'')
+pat = re.compile(r'\b[aeiou][\w]+', re.I)
+# print(re.findall(pat, string4))
+
+lst = ['010-256-1354', '010-1234-5576', '070-642-0384', '010-290*-4858', '0105734123']
+pat = re.compile(r'010-\d{3,4}-\d{4}')
+result = filter(pat.match, lst)
+for l in lst:
+    if l in result:
+        print(l, 'Yes')
+    else:
+        print(l, 'No')
