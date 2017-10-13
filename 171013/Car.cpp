65e8c4a99192fe1774c8d8f2fb0b4034148b5877
rnaster <blpomspo@gmail.com>
2017-10-14 03:48:02 +0900

    cpp
    복습이 필요...

---
+++ 171013/Car.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstring>
+#include "Car.h"
+using namespace std;
+
+void Car::InitMembers(const char * ID, int fuel)
+{
+	strcpy(gamerID, ID);
+	fuelGauge = fuel;
+	curSpeed = 0;
+}
+
+void Car::ShowCarState()
+{
+	cout<<"OwnerID: "<<gamerID<<endl;
+	cout<<"Amount of fuel: "<<fuelGauge<<'%'<<endl;
+	cout<<"Current Velocity: "<<curSpeed<<"km/s"<<endl<<endl;
+}
+
+void Car::Accel()
+{
+	if (fuelGauge <= 0)
+		return;
+	else
+		fuelGauge -= CAR_CONST::FUEL_STEP;
+
+	if ((curSpeed + CAR_CONST::ACC_STEP) >= CAR_CONST::MAX_SPD)
+	{
+		curSpeed = CAR_CONST::MAX_SPD;
+		return;
+	}
+	curSpeed += CAR_CONST::ACC_STEP;
+}
+
+void Car::Break()
+{
+	if (curSpeed < CAR_CONST::BRK_STEP)
+	{
+		curSpeed = 0;
+		return;
+	}
+	curSpeed -= CAR_CONST::BRK_STEP;
+}
