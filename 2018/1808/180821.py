ac728692ccf040b9fdacee9d21fbbf5252634059
yunseop <blpomspo@gmail.com>
2018-08-22 00:32:32 +0900

    모르겠다,,

--- 2018/1808/180821.py
+++ 2018/1808/180821.py
@@ -2,20 +2,21 @@
 # l = list(range(1, 1000001))
 # v = sum(map(lambda x: x ** 2, l)) / len(l) - (sum(l) / len(l)) ** 2
 # print(v / len(l))
-ans = 83334
+import math
+ans = math.inf
 n, k = map(int, input().split())
-if k == 1: print(0); exit()
 tp = tuple(map(int, input().split()))
+tp_ = tuple(map(lambda x: x * x, tp))
 for i in range(n-k+1):
-    m = sum(tp[i:i+k]) / k
-    v = sum(map(lambda x: x ** 2, tp[i:i+k])) / k - m ** 2
-    ans = min(ans, v ** (0.5))
-
+    for j in range(k):
+        m = sum(tp[i:i+k+j]) / (k + j)
+        v = sum(tp_[i:i+k+j]) / (k + j) - m * m
+        ans = min(ans, math.sqrt(v))
 print(ans)
 """
 5 3
 1 2 3 4 5
-10 1
+10 3
 1 4 1 5 9 2 6 5 3 5
 0.9428090415820655
 """
